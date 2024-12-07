88ff2ee9ade0d82f7c19b9fa4d04b089628ce849
Sze Kai Low <lszekai977@gmail.com>
2024-12-07 17:46:58 +1100

    new version

--- function.py
+++ function.py
@@ -1,12 +1,25 @@
 from googleapiclient.discovery import build
+from google.cloud import bigquery
 
-def trigger_df_job():   
+def trigger_df_job(cloud_event, environment):   
     
     # Initializing the Dataflow API Service
     service = build('dataflow', 'v1b3')
 
+    # Initialize BigQuery client
+    bq_client = bigquery.Client()
+
     # Setting the project ID
-    project = "cricket-statistics-etl"
+    project_id = "cricket-statistics-etl"
+
+    # BigQuery table details
+    dataset_id = "cricket_dataset"
+    table_id = "icc_odi_bastman_ranking"
+    table = f"{project_id}.{dataset_id}.{table_id}"
+
+    # Step 1: Clear the BigQuery table
+    delete_query = f"DELETE FROM `{table}` WHERE TRUE"
+    bq_client.query(delete_query).result()  # Execute query and wait for it to finish
 
     # Points to a pre-built Dataflow template provided by Google
     template_path = "gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery"
@@ -18,13 +31,13 @@ def trigger_df_job():
         "JSONPath": "gs://bucket-dataflow-required-data/bq.json",
         "javascriptTextTransformFunctionName": "transform",
         "outputTable": "cricket-statistics-etl:cricket_dataset.icc_odi_bastman_ranking",
-        "inputFilePattern": "bucket-cricket-data/batsmen_rankings.csv",
-        "bigQueryLoadingTemporaryDirectory": "bucket-dataflow-required-data",
+        "inputFilePattern": "gs://bucket-cricket-data/batsmen_rankings.csv",
+        "bigQueryLoadingTemporaryDirectory": "gs://bucket-dataflow-required-data"
         }
     }
 
-    # Launching the Dataflow Job
-    request = service.projects().templates().launch(projectId=project, gcsPath=template_path, body=template_body)
+    # Step 2: Launch the Dataflow Job
+    request = service.projects().templates().launch(projectId=project_id, gcsPath=template_path, body=template_body)
     response = request.execute()
-    
+
     print(response)
